ee07aa64209e837d7cc7c919704c1edfc46158dc
Julian Gonzalez <juliangonzalez.code@gmail.com>
2017-05-09 14:52:17 -0500

    configured factory girl generator

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ module BoilerplateRailsApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
